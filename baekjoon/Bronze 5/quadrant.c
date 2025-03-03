5a6568248164a8b178c62365e57a2aabab1fb7a6
MaeHoo <maehoo321@gmail.com>
2025-03-03 23:23:55 +0900

    [14681] 사분면 고르기

---
+++ baekjoon/Bronze 5/quadrant.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A,B;
+    scanf("%d %d", &A, &B);
+    if(A>=0)
+    {
+        if(B>=0)
+        printf("1");
+        else
+        printf("4");
+    }
+    else
+    {
+        if(B>=0)
+        printf("2");
+        else
+        printf("3");
+    }
+    return 0;
+}
