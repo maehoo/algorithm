bae1bf52297f5e4e0699875e85510e86fcc0a94d
MaeHoo <maehoo321@gmail.com>
2025-03-08 00:21:57 +0900

    [25314] 코딩은 체육과목 입니다.

---
+++ baekjoon/Bronze 5/longint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    int i;
+    for(i=1; i<=N/4;i++)
+    {
+        printf("%s ","long");
+    }
+    printf("int");
+    return 0;
+}
