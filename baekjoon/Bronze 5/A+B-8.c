d2299b61d1a703d6101ed048fe7fcc5aabc1e255
MaeHoo <maehoo321@gmail.com>
2025-03-08 16:36:40 +0900

    [11022] A+B-8

--- baekjoon/Bronze 5/A+B-8.c
+++ baekjoon/Bronze 5/A+B-8.c
@@ -8,7 +8,7 @@ int main(void)
     for(i=1;i<=A;i++)
     {
         scanf("%d %d",&B,&C);
-        printf("Case #%d : %d + %d = %d\n",i,B,C,B+C);
+        printf("Case #%d: %d + %d = %d\n",i,B,C,B+C);
     }
     return 0;
 }
