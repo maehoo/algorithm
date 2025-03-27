232b6b34074dc17af54f5eac96f95bd8ec04ddca
MaeHoo <maehoo321@gmail.com>
2025-03-27 14:07:32 +0900

    [3003] 킹,퀸,룩,비숍,나이트,폰

---
+++ baekjoon/Bronze 5/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a=1,b=1,c=2,d=2,e=2, f=8;
+    int A,B,C,D,E,F;
+
+    scanf("%d %d %d %d %d %d",&A,&B,&C,&D,&E,&F);
+    printf("%d %d %d %d %d %d",a-A,b-B,c-C,d-D,e-E,f-F);
+    return 0;
+}
