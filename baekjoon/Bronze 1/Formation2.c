58f2d4867fee26ff1e291b114e18eee899598f32
MaeHoo <maehoo321@gmail.com>
2025-04-06 00:28:01 +0900

    [11005] 진법 변환2

---
+++ baekjoon/Bronze 1/Formation2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A,B,Z,i;
+    char arr[10000];
+    scanf("%d %d",&A,&B);
+    while(A!=0)
+    {
+    
+        Z=A%B;
+        A=A/B;
+        if(Z>=10)
+        arr[i]=Z+55;
+        else
+        arr[i]=Z+48;
+        i++;
+    }
+    i -=1;
+    for(;i>=0;i--)
+    {
+        printf("%c",arr[i]);
+    }
+    return 0;
+}
