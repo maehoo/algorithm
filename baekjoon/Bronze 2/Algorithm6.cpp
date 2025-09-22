bab2ccb18c160ab373bdca66e5cb961a612a7dc4
MaeHoo <maehoo321@gmail.com>
2025-09-22 13:11:37 +0900

    [24267] 알고리즘 수업 - 알고리즘 수행 시간 6

---
+++ baekjoon/Bronze 2/Algorithm6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    long long a;
+    cin >>a;
+    cout << a * (a-1) * (a-2) / 6 << endl;
+    cout << "3"<<endl;
+    return 0; 
+}
