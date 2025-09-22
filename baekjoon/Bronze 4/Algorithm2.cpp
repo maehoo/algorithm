addbce4eddb1a4b0b0988ca8226b16167eeaeed1
MaeHoo <maehoo321@gmail.com>
2025-09-22 12:28:58 +0900

    [24263] 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ baekjoon/Bronze 4/Algorithm2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a;
+    cin >> a;
+    int sum =0;
+    int i;
+    for(i=0;i<a;i++)
+    {
+        sum++;
+    }
+    cout<<sum<<'\n'<<"1"<<endl;
+    return 0;
+}
